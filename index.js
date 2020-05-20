1002afddcbc660da9adc00b731d319ed21edc4dc
Manjari <jlmanjari@gmail.com>
2020-05-19 23:32:51 -0300

    changed package.json

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 const app = express();
 
+
+// simple GET api
 app.get('/home' , function(req, res) {
     res.send("Hello world!");
 });
 
-
 app.listen(3000, function() {
     console.log("Listening on port 3000");
 });
+
+
+
